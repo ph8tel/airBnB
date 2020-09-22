d8ccc4a75b77bc5777731d696ec4395a1cb8988e
Joe Jackson <ph8tel@gmail.com>
2020-09-22 00:34:52 -0700

    trimming

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require('express')
 const app = express()
 const port = process.env.port || 3333
-const db = require('./mongo')
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')));
+const keys = require('./keys')
 
+const encodedPass = encodeURIComponent(keys.pass)
+const uri = `mongodb+srv://ph8l:${encodedPass}@cluster0.kl61z.mongodb.net/?retryWrites=true&w=majority`
 
-app.get( '/', (req,res) => {
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(uri);
+
+app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-app.listen( port, console.log(`running on port ${port}`) )
+app.get('/air', async(req, res) => {
+
+    async function run() {
+        try {
+            await client.connect();
+
+            const database = client.db("sample_airbnb");
+            const collection = database.collection("listingsAndReviews");
+
+            const query = { _id: "1001265" };
+
+            const listing = await collection.findOne(query);
+            // since this method returns the matched document, not a cursor, print it directly
+            res.json(listing)
+
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(err => {
+        console.dir
+        res.json(err)
+    });
+})
+
+app.listen(port, console.log(`running on port ${port}`))
--- mongo.js
+++ mongo.js
@@ -1,10 +1,28 @@
-const MongoClient = require('mongodb').MongoClient;
 const keys = require('./keys')
-const uri = `mongodb+srv://ph8l:${keys.pass}@cluster0.kl61z.mongodb.net/Cluster0?retryWrites=true&w=majority`
-const client = new MongoClient(uri, { useNewUrlParser: true })
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-module.exports = client
+const encodedPass = encodeURIComponent(keys.pass)
+const uri = `mongodb+srv://ph8l:${encodedPass}@cluster0.kl61z.mongodb.net/?retryWrites=true&w=majority`
+
+const { MongoClient } = require("mongodb");
+
+
+
+// Create a new MongoClient
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+async function run() {
+    try {
+        // Connect the client to the server
+        await client.connect();
+
+        // Establish and verify connection
+        await client.db("sample_airbnb").command({ ping: 1 });
+
+        console.log("Connected successfully to server");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+module.exports = client;
